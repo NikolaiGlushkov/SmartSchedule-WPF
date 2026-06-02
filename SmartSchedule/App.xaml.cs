d902561d857f6ebce4f8f2fed7cf32272add1fbb
Nikolai_Glushkov <glushkovnikolayy@gmail.com>
2026-06-02 20:44:36 +0300

    fix(app): restrict application to a single instance using Mutex

--- SmartSchedule/App.xaml.cs
+++ SmartSchedule/App.xaml.cs
@@ -11,8 +11,19 @@ namespace SmartSchedule
     /// </summary>
     public partial class App : Application
     {
+        // Prevent multiple instances of the app from running simultaneously
+        private static readonly Mutex _mutex = new Mutex(true, "SmartSchedule-Unique-System-Mutex-Key-12345");
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            if (!_mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                MessageBox.Show("The application is already running!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Current.Shutdown();
+                return;
+            }
+
+
             base.OnStartup(e);
 
             FrameworkElement.LanguageProperty.OverrideMetadata(
